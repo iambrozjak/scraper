28fb79bcd6a763a18ece6628dcf08073f6d014ff
iambrozjak <ivanochkavasyljuk@gmail.com>
2024-02-13 19:50:07 +0200

    Addded changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,11 @@ class HomeController < ApplicationController
 
   def index
     @url = params[:url]
-    @scraper = scrape if @url.present?
+    if @url.present?
+      @scraper = scrape
+    else
+      render :index, status: :unprocessable_entity, alert: "Введіть URL"
+    end
   end
 
   private
@@ -12,4 +16,11 @@ class HomeController < ApplicationController
     scraper = Scraper.new(@url)
     scraper.scrape
   end
+
+  def authenticate_user!
+    unless user_signed_in?
+      redirect_to new_user_session_path, alert: "Please sign in to continue"
+    end
+  end
+
 end
